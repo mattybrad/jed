77607ab67b2f3d52d528ce456d9ac9550e7b0450
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2017-03-10 14:12:39 +0000

    dummy interval thing

--- src/components/Singer/index.js
+++ src/components/Singer/index.js
@@ -1,11 +1,21 @@
 export default class Singer {
   constructor() {
     this.wordQueue = [];
-    console.log("singer initialised");
+    this.currentWord = null;
+    setInterval(this.nextWord.bind(this), 1000); // using an interval for now to simulate flow of words
   }
 
   addToWordQueue(words) {
     this.wordQueue = this.wordQueue.concat(words);
     console.log(this.wordQueue);
+  }
+
+  nextWord() {
+    if(this.wordQueue.length) {
+      this.currentWord = this.wordQueue.shift();
+    } else {
+      this.currentWord = null;
+    }
+    console.log(this.currentWord);
   }
 }
